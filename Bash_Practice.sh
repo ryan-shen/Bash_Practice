f9b11730571c68571a16831fec690ccbc733f25d
zx1230516 <zx1230516@gmail.com>
2017-08-30 00:44:34 +0800

    Add files via upload

---
+++ Bash_Practice.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#----
+# Written by Wen Shen on 2017/8/29
+#----
+
+limitTime=60
+interval=5
+
+# Create a directory and create a file into it every five seconds.
+mkdir testDirectory
+for (( i=1; i<=$(( limitTime / interval )); i++ ))
+do
+    sleep ${interval}
+    touch testDirectory/$(date +%Y年%m月%d日_%H點%M分%S秒)
+done
+
+# List all files in testDirectory
+ls testDirectory/ > listFiles
